6a3db2907102447d6b9d566e9c465e6dc1f981c0
Prajwal264 <prajwalp264@gmail.com>
2022-01-10 19:58:31 +0530

    (feat): added list page UI

--- pages/teams/index.tsx
+++ pages/teams/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Avatar from 'react-avatar';
+import Header from '../../components/shared/header/header.component';
+import styles from '../../styles/pages/team-list.module.scss';
 
 interface Props {
 
@@ -8,8 +11,30 @@ const TeamsPage: React.FC<Props> = ({
 
 }) => {
   return (
-    <div>
-
+    <div className={styles.teamListPage}>
+      <Header />
+      <h4 className={styles.heading}>
+        Teams you belong to.
+      </h4>
+      <div className={styles.cardList}>
+        <div className={styles.card}>
+          <Avatar name='team' />
+          <div className={styles.content}>
+            <p className={styles.name}>
+              #&nbsp;team name
+            </p>
+            <div className={styles.details}>
+              <span>
+                2 members
+              </span>
+              &nbsp;&nbsp;·&nbsp;&nbsp;
+              <span>
+                This channel is for working on a project. Hold meetings, share docs, and make decisions together with your team.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
